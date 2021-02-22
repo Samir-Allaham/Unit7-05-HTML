9add7970cb39a84d8bdb33db7c8d2c812fbfd6b2
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-22 18:15:25 +0000

    spaces

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function movie () {
   day = document.getElementById('hello').value
   if ((age < 18) && (day === 'Monday' || day === 'Friday')) {
     document.getElementById('answer').innerHTML = 'Time for school'
-  } else if ((age >= 18)  && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Friday' || day === 'Thursday')) { 
+  } else if ((age >= 18) && (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Friday' || day === 'Thursday')) {
     document.getElementById('answer').innerHTML = 'Time for work'
-  } else if ((age >= 18)  && (day === 'Sunday' || day === 'Saturday')) {
-     document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
+  } else if ((age >= 18) && (day === 'Sunday' || day === 'Saturday')) {
+    document.getElementById('answer').innerHTML = 'Time to relax for the weekend'
   }
-   document.getElementById('movie').innerHTML = 'Thank for verifying your age and day of the week!'
+  document.getElementById('movie').innerHTML = 'Thank for verifying your age and day of the week!'
 }
